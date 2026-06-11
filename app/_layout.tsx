778e8d77d462b5b501d58368dfbddc4a886affe5
Nnavodya <rajapakse22030.kln@gmail.com>
2026-06-11 13:06:25 +0530

    feat: add root layout with Stack navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="attractions" options={{ headerShown: false }} />
+        <Stack.Screen name="details" options={{ headerShown: false }} />
+        <Stack.Screen name="favorites" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
 }
