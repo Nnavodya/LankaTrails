82f8f415814ffd80b5add47ab0343b55042203d2
Nnavodya <rajapakse22030.kln@gmail.com>
2026-06-11 23:32:24 +0530

    refactor: add shared Attraction type definition

---
+++ types/Attraction.ts
@@ -0,0 +1,11 @@
+export interface Attraction {
+  id: string;
+  name: string;
+  category: string;
+  description: string;
+  image: any;
+  latitude: number;
+  longitude: number;
+  location: string;
+  rating: number;
+}
